43e140e6a791f77faad1dc4f923ab2b7323c1389
“JoDeLeon95” <JoDeLeon95@gmail.com>
2019-02-28 20:46:06 -0700

    Modified buttons, moved buttons to bottom, and added onClick.

--- app/src/main/java/edu/cnm/deepdive/fractapp/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/fractapp/MainActivity.java
@@ -2,12 +2,17 @@ package edu.cnm.deepdive.fractapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+  public void sendMessage(View view) {
+
+  }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
+
 }
